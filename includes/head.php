f431da40942e55bc8606f57eb0073ac4376cf1c7
Marc L <mlorentet@esliceu.net>
2022-05-10 11:42:31 +0200

    Insertar Nou Client en BBDD

--- includes/head.php
+++ includes/head.php
@@ -1,15 +1,13 @@
+<?php
+require 'includes/database.php'
+?>
 
-  <?php 
-  require 'includes/database.php'
-  ?>
 <head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/style.css" />
   <meta charset="utf-8" />
   <title>Pàgina de test</title>
-  <meta
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-      name="viewport"
-  />
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
   <meta content="" name="description" />
   <meta content="" name="author" />
 </head>
