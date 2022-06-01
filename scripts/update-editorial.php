911d0d71c8a609ed66e660711a5d5e0f7968e09c
Marc L <mlorentet@esliceu.net>
2022-06-01 11:38:27 +0200

    Llista de països en català + afegit camp "Nom" taula editorial

--- scripts/update-editorial.php
+++ scripts/update-editorial.php
@@ -2,6 +2,7 @@
 error_reporting(E_ERROR | E_PARSE);
 require '../includes/database.php';
 
+$Nom = $_POST['name'];
 $Ciutat = $_POST['city'];
 $Direccio = $_POST['address'];
 $CP = $_POST['CP'];
@@ -9,7 +10,7 @@ $Provincia = $_POST['province'];
 $Pais = $_POST['country'];
 $id = $_POST['id'];
 
-$query = "UPDATE Editorial SET Ciutat = '$Ciutat', Direccio = '$Direccio', CP = '$CP', Provincia = '$Provincia', Pais = '$Pais' WHERE id = '$id' ";
+$query = "UPDATE Editorial SET Nom = '$Nom', Ciutat = '$Ciutat', Direccio = '$Direccio', CP = '$CP', Provincia = '$Provincia', Pais = '$Pais' WHERE id = '$id' ";
 
 $result = mysqli_query($dbh, $query);
 
