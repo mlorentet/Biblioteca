6f6ab5a4840f3eed576dd3f5540162d74c9699db
JT <jrodriguezn@esliceu.net>
2022-06-03 09:09:42 +0200

    Footer en català

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="mt-auto bg-dark text-left text-white">
         <h7>
             Pàgina creada per Marc Lorente i Joan Toni Rodríguez per a provar els llenguatges HTML/CSS
-            <br>© 2022 Marc Lorente Tur i Joan Toni Rodríguez Nicolau Todos los derechos reservados.</br>
+            <br>© 2022 Marc Lorente Tur i Joan Toni Rodríguez Nicolau tots els drets reservats.</br>
         </h7>
     </footer>
