4442ef361cf93df7df57ead7eb301065d3b1cdda
Marc <mlorentet@esliceu.net>
2022-06-10 10:32:25 +0200

    Formulari Registre/Login formatats

--- login.php
+++ login.php
@@ -4,47 +4,51 @@
 <head>
     <title>Formulari Login</title>
 </head>
-<?php include 'includes/head.php' ?>
+<?php include 'includes/head.php'; ?>
 
 <body class="d-flex flex-column min-vh-100">
-    <?php include 'includes/header.html' ?>
-    <div class="log-form">
-        <h2>
-            Inicia Sessió amb el teu compte
-        </h2>
-        <form>
-            <div class="form-group">
-                <fieldset>
-                    <label for="Usuari">Usuari</label>
-                    <input type="text" placeholder="Usuari" required />
-                </fieldset>
-            </div>
-            <div class="form-group">
-                <fieldset>
-                    <label for="Contrasenya">Contrasenya</label>
-                    <input type="password" id="myInput" placeholder="Contrasenya">
-                </fieldset>
-            </div>
-            <div>
-                <input type="checkbox" onclick="myFunction()">Mostra la Contrasenya
-                <script>
-                    function myFunction() {
-                        var x = document.getElementById("myInput");
-                        if (x.type === "password") {
-                            x.type = "text";
-                        } else {
-                            x.type = "password";
+    <section>
+        <?php include 'includes/header.html' ?>
+    </section>
+    <div class="container" style="display: flex; align-items: center; justify-content: center;">
+        <section class="mb-5 mt-5">
+            <h2>
+                Inicia Sessió amb el teu compte
+            </h2>
+            <form>
+                <div class="form-group">
+                    <fieldset>
+                        <label for="Usuari">Usuari</label>
+                        <input type="text" class="form-control" placeholder="Usuari" required />
+                    </fieldset>
+                </div>
+                <div class="form-group">
+                    <fieldset>
+                        <label for="Contrasenya">Contrasenya</label>
+                        <input type="password" class="form-control" id="myInput" placeholder="Contrasenya">
+                    </fieldset>
+                </div>
+                <div>
+                    <input type="checkbox" onclick="myFunction()">Mostra la Contrasenya
+                    <script>
+                        function myFunction() {
+                            var x = document.getElementById("myInput");
+                            if (x.type === "password") {
+                                x.type = "text";
+                            } else {
+                                x.type = "password";
+                            }
                         }
-                    }
-                </script>
-            </div>
-            <div class="login-reset-button">
-                <button type="reset" class="btn btn-danger">Reiniciar</button>
-            </div>
-            <div class="login-submit-button">
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </div>
-        </form>
+                    </script>
+                </div>
+                <div class="login-reset-button">
+                    <button type="reset" class="btn btn-danger">Reiniciar</button>
+                </div>
+                <div class="login-submit-button">
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </div>
+            </form>
+        </section>
     </div>
     <?php include 'includes/footer.php' ?>
 </body>
